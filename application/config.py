4f1707eb8ab052e558b37a46fbc5c0409803e08e
Sharkum <sharan342.kumar@gmail.com>
2022-11-10 13:01:35 +0530

    Started on kanban application

--- application/config.py
+++ application/config.py
@@ -7,7 +7,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config():
     DEBUG = False 
     SQLITE_DB_DIR = None
+    SQLALCHEMY_DATABASE_URI = None
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    SECURITY_TOKEN_AUTHENTICATION_HEADER = "Authentication-Token"
 
 class LocalDevelopmentConfig(Config):
     SQLITE_DB_DIR = os.path.join(basedir,"../db_directory")
--- application/models.py
+++ application/models.py
@@ -1,10 +1,11 @@
 from sqlalchemy import ForeignKey
 from .database import db
+from sqlalchemy.orm import declarative_base, relationship
 
 class Logs(db.Model):
     __tablename__ = 'Logs'
     LogID = db.Column(db.Integer, autoincrement= True, primary_key=True)
-    UserName = db.Column(db.String, nullable=False)
+    UserName = db.Column(db.String, ForeignKey('User.UserName'), nullable=False)
     Tracker_name = db.Column(db.String, nullable=False)
     Date_created = db.Column(db.String, nullable=False)
     Last_modified = db.Column(db.String, nullable=False)
@@ -22,3 +23,5 @@ class User(db.Model):
     __tablename__ = 'User'
     UserName = db.Column(db.String, nullable=False, unique=True, primary_key=True)
     Password = db.Column(db.String, nullable=False)
+
+    logs = relationship('Logs')
