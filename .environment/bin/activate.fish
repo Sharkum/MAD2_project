267fc463582d483a5988185c2024781687f053b2
Sharkum <sharan342.kumar@gmail.com>
2022-11-26 13:03:24 +0530

    started working on login

--- .environment/bin/activate.fish
+++ .environment/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/sharan/Desktop/python/MAD_redo/.environment"
+set -gx VIRTUAL_ENV "/home/sharan/Desktop/python/MAD2_project/.environment"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
